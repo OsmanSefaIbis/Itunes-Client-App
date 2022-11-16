fead984fad1324eded4a76c03566aad4f7c439d5
Sefa İbiş <>
2022-11-16 19:45:36 +0300

    main view added

--- Itunes Client App/Itunes Client App/Screens/Main/Controller/MainViewController.swift
+++ Itunes Client App/Itunes Client App/Screens/Main/Controller/MainViewController.swift
@@ -11,8 +11,8 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view = MainView()
+    
     }
 
 
---
+++ Itunes Client App/Itunes Client App/Screens/Main/View/Base.lproj/MainView.swift
@@ -0,0 +1,13 @@
+//
+//  MainView.swift
+//  Itunes Client App
+//
+//  Created by Sefa İbiş on 16.11.2022.
+//
+
+import UIKit
+
+final class MainView: UIView {
+
+
+}
