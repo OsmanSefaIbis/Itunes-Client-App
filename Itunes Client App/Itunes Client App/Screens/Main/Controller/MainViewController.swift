9c092e163848b1bacbc319c3f7fcb664715bafaa
Sefa İbiş <>
2022-11-16 19:32:57 +0300

    configured the project

--- Itunes Client App/Itunes Client App/Screens/Main/Controller/MainViewController.swift
+++ Itunes Client App/Itunes Client App/Screens/Main/Controller/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Itunes Client App
 //
 //  Created by Sefa İbiş on 16.11.2022.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
